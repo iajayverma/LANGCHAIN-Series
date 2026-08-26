c1178827d88afcaac2419aea851efbc2d2541475
iajayverma <ajay.verma@simplyphi.co.uk>
2026-08-26 17:11:19 +0530

    day1

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 import os
+
 load_dotenv()
+
 
 def main():
     print("Hello from langchain-course")
     print(os.getenv("OPENAI_API_KEY"))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
